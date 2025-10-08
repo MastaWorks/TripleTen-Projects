1d7d57240b386a3b233defb354f118f9f35f9d96
Tanveer Efty <tanveer.maink@gmail.com>
2025-10-08 14:21:05 -0400

    Progress

--- Sprint 4/Functions.js
+++ Sprint 4/Functions.js
@@ -26,8 +26,40 @@ console.log("EXERCISE 2");
 
 let year = 2020;
 
-if (year % 400 === 0 || (year % 4 === 0 && !(year % 100 === 0))) {
+function isLeapYear(year) {
+  if (year % 400 === 0 || (year % 4 === 0 && !(year % 100 === 0))) {
   console.log(year + " is a leap year.");
 } else {
   console.log(year + " is a non-leap year.");
 }
+};
+
+isLeapYear(2012);
+
+//EXERCISE 3
+console.log('EXERCISE 3');
+
+// Now it’s time for you to write a function from scratch! Write a function called calculateArea. This function should accept two arguments representing the length and width of a rectangle. The function's body should calculate its area using the formula length * width, and log the result to the console.
+
+// Declare the calculateArea function
+
+function calculateArea(num1, num2){
+  console.log(num1 * num2);
+}
+
+calculateArea(5, 10); // 50
+calculateArea(10, 5); // 50
+calculateArea(3, 3); // 9
+
+//Returning Result/Output rather than just printing it out on the console
+
+function isLeapYear2(year) {
+  if (year % 400 === 0 || (year % 4 === 0 && !(year % 100 === 0))) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let checkedResult = isLeapYear2(2020);
+console.log(checkedResult);
