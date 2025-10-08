0c538336d3fe3a91cfd71dbb0c5e473df7b416e5
Tanveer <tanveer.maink@gmail.com>
2025-10-07 23:47:04 -0400

    New Notes

---
+++ Sprint 4/Objects.js
@@ -0,0 +1,15 @@
+//EXERCISE 1
+console.log("EXERCISE 1");
+
+let house = {
+  price: 200000,
+};
+
+let buyer = {
+  name: "Mike",
+  money: 300000,
+};
+
+console.log(buyer.money);
+console.log(house.price);
+console.log(buyer.money >= house.price);
