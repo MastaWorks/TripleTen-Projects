2e48d8ae3e82e9fae844469af123c027913877d4
Tanveer <tanveer.maink@gmail.com>
2025-10-13 02:28:55 -0400

    to be continued

--- Sprint 4/Connecting HTML and Javascript/script.js
+++ Sprint 4/Connecting HTML and Javascript/script.js
@@ -2,7 +2,11 @@ const reservationButton = document.querySelector(".reservation__button");
 const reservationModal = document.querySelector("#reservation-modal");
 const reservationCloseBtn = reservationModal.querySelector(".modal__close-btn");
 const reservationForm = reservationModal.querySelector(".modal__form");
-const confirmatiionModal= document.querySelector('#confirmation-modal');
+const confirmatiionModal = document.querySelector("#confirmation-modal");
+const reservationNameInput = reservationModal.querySelector("#name-input");
+const reservationCountInput = reservationModal.querySelector("#count-input");
+const reservationDateInput = reservationModal.querySelector("#datetime-input");
+const reservationEmailInput = reservationModal.querySelector("#email-input");
 
 // reservationButton.addEventListener("click", function () {
 //   reservationModal.setAttribute("style", "visibility: visible");
@@ -20,7 +24,15 @@ reservationCloseBtn.addEventListener("click", function () {
   reservationModal.removeAttribute("class", "modal_is-opened");
 });
 
-reservationForm.addEventListener('submit', function(evt){
+reservationForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
-  confirmatiionModal.classList.add('modal_is-opened');
+  confirmationModal.classList.add("modal_is-opened");
+  reservationModal.classList.remove("modal_is-opened");
+
+  console.log(reservationNameInput.value);
+  console.log(reservationCountInput.value);
+  console.log(reservationDateInput.value);
+  console.log(reservationEmailInput.value);
+
+  reservationForm.reset();
 });
